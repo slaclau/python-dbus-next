3e68f099cd98522b31ec150cb38e2edec016260c
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-27 22:43:27 +0100

    Create updateChangelog.py

---
+++ updateChangelog.py
@@ -0,0 +1,8 @@
+import distro
+import fileinput
+
+codename = distro.codename()
+
+for line in fileinput.input("debian/changelog", inplace=True):
+    print(line.replace("replaceme", codename), end="")
+Give feedback
