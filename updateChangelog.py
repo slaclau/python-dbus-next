874014fd84704ff1a4b91ec860d05712e9ecd131
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-28 09:52:10 +0100

    Update updateChangelog.py

--- updateChangelog.py
+++ updateChangelog.py
@@ -4,4 +4,4 @@ import fileinput
 codename = distro.codename()
 
 for line in fileinput.input("debian/changelog", inplace=True):
-    print(line.replace("replaceme", codename), end="")
+    print(line.replace(") replaceme", "~" + codename + ") " + codename), end="")
